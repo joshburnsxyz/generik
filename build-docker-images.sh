912ba09721464c674b2cad4f2f89401a83c2aa41
Josh Burns <joshyburnss@gmail.com>
2024-12-30 21:07:19 +1100

    script to build docker images and publish them

---
+++ build-docker-images.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+TAGVAL=0.1.0
+
+# AMD64
+docker buildx build --platform linux/amd64 -t joshburnsxyz/generik:latest .
+docker buildx build --platform linux/amd64 -t joshburnsxyz/generik:$TAGVAL .
+
+# ARM64
+docker buildx build --platform linux/arm64 -t joshburnsxyz/generik:latest-arm .
+docker buildx build --platform linux/arm64 -t joshburnsxyz/generik:$TAGVAL-arm .
+
+# Publish
+docker push -a joshburnsxyz/generik
